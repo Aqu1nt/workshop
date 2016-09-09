731aa19db6a14d489cf696c317da6aeb286220bd
Sandro Schweizer, PF <sandro.schweizer@post.ch>
2016-09-09 07:48:43 +0200

    Improved browserify build

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,11 @@ elixir((mix) => {
     babelConfig.options.presets = ["es2015", "stage-2"];
 
     //Transpile JS
-    mix.browserify('./src/js/Workshop.js');
+    mix.browserify('./src/js/Workshop.js', undefined, undefined, {
+        packageCache : {},
+        cache : {},
+        debug : true
+    });
 
     //Compile sass
     mix.sass('./src/sass/workshop.scss', undefined, undefined, {
